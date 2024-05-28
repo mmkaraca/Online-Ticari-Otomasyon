fed9aa69de8bc122518aa76f360f63eb49ee06ce
mmkaraca <mustafamuratkaraca@gmail.com>
2024-05-28 20:07:19 +0300

    ilk

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Carilers.Where(x=>x.Durum==true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniCari()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCari(Cariler p)
+        {
+            p.Durum = true;
+            c.Carilers.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariSil(int id)
+        {
+            var cari = c.Carilers.Find(id);
+            cari.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariGetir(int id)
+        {
+            var cr = c.Carilers.Find(id);
+            
+            return View("CariGetir",cr);
+        }
+        public ActionResult CariGuncelle(Cariler p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+            var cr = c.Carilers.Find(p.Cariid);
+            cr.CariAd = p.CariAd;
+            cr.CariSoyad = p.CariSoyad;
+            cr.CariSehir = p.CariSehir;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriSatis(int id)
+        {
+            var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            var cr = c.Carilers.Where(x => x.Cariid == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.cari = cr;
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -0,0 +1,43 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            var grafikciz = new Chart(600, 600);
+            grafikciz.AddTitle(text: "Kategori-Ürün Stok Sayısı").AddLegend("Stok").AddSeries
+                ("Değerler", xValue: new[] { "Beyaz Eşya", "Televizyon", "Bilgisayar", "Küçük Ev Aletleri" }, yValues: new[] { 500, 250, 340, 620 }).Write();
+
+            
+            return File (grafikciz.ToWebImage().GetBytes(),"image/jpeg");
+        }
+        Context c = new Context();
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var sonuclar = c.Uruns.ToList();
+            sonuclar.ToList().ForEach(x => xvalue.Add(x.UrunAd));
+            sonuclar.ToList().ForEach(y => yvalue.Add(y.Stok));
+            var grafik = new Chart(width: 500, height: 500)
+                .AddTitle("Stoklar")
+                .AddSeries(chartType: "Column", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafik)
+        }
+    }
+
+}
