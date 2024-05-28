7dc18c6097905606bdfe51e5e7aa2d729ce054ea
mmkaraca <mustafamuratkaraca@gmail.com>
2024-05-29 02:26:36 +0300

    a

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -26,6 +27,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.Cariid).FirstOrDefault();
             var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
             return View(degerler);
+        }
+        public ActionResult KargoTakip(string p)
+        {
+            var k = from x in c.KargoDetays select x;
+          
+            
+                k = k.Where(y => y.TakipKodu.Contains(p));
+            
+            return View(k.ToList());
+        }
+        public ActionResult CariKargoTakip(string id)
+        {
+            var degerler = c.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+
+            return View(degerler);
+
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
--- MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -33,11 +33,85 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var sonuclar = c.Uruns.ToList();
             sonuclar.ToList().ForEach(x => xvalue.Add(x.UrunAd));
             sonuclar.ToList().ForEach(y => yvalue.Add(y.Stok));
-            var grafik = new Chart(width: 500, height: 500)
+            var grafik = new Chart(width: 1000, height: 1000)
                 .AddTitle("Stoklar")
-                .AddSeries(chartType: "Column", name: "Stok", xValue: xvalue, yValues: yvalue);
-            return File(grafik)
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafik.ToWebImage().GetBytes(), "image/jpeg");
         }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(Urunlistesi(), JsonRequestBehavior.AllowGet);
+        }
+        public List<sinif1> Urunlistesi()
+        {
+            List<sinif1> snf = new List<sinif1>();
+            snf.Add(new sinif1()
+            {
+                urunad = "Bilgisayar",
+                stok = 120
+            });
+            snf.Add(new sinif1()
+            {
+                urunad="Beyaz Eşya",
+                stok=150
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobil Cihazlar",
+                stok = 90
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobilya",
+                stok = 70
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Küçük Ev Aletleri",
+                stok = 180
+            });
+            return snf;
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeUrunResult2()
+        {
+            return Json(Urunlistesi2(), JsonRequestBehavior.AllowGet);
+        }
+        public List<sinif2> Urunlistesi2()
+        {
+            List<sinif2> snf = new List<sinif2>();
+            using (var c = new Context())
+            {
+                snf = c.Uruns.Select(x => new sinif2
+                {
+                    urn = x.UrunAd,
+                    stk = x.Stok
+                }).ToList();
+                return snf;
+            }
+        }
+        public ActionResult Index6()
+        {
+            return View();
+        }
+        public ActionResult Index7()
+        {
+            return View();
+        }
+
+
+
     }
+
+
 
 }
